6afb38c4dabb42322f393cd9caa988c5bd27277f
LinZeyuan <903586678@qq.com>
2025-06-05 14:39:26 +0800

    llm_begin_3

--- demo/seq2seq_demo.py
+++ demo/seq2seq_demo.py
@@ -1,15 +1,14 @@
 import os
+from datasets import load_dataset
+from transformers import T5Tokenizer, T5ForConditionalGeneration, Seq2SeqTrainer, Seq2SeqTrainingArguments
 
-import torch
-from datasets import Dataset
-from transformers import (
-    LlamaTokenizer,
-    LlamaForCausalLM,
-    Trainer,
-    TrainingArguments,
-    DataCollatorForLanguageModeling,
-)
-from peft import LoraConfig, get_peft_model
+# 加载数据集
+data = load_dataset('../data/opus_books', 'en-fr')
+print(data)
+
+# 加载分词器和模型
+t5_model = T5ForConditionalGeneration.from_pretrained('/home/ps/lzy/kg/pre_trained/t5-small')
+t5_tokenizer = T5Tokenizer.from_pretrained('/home/ps/lzy/kg/pre_trained/t5-small')
 
 """
     Seq2SeqTrainer: 训练的模型必须包含Encoder-Decoder（编码器-解码器）结构, 支持 .generate() 方法并返回包含 .loss() 的输出
