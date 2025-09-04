7616f557869fe8c63473fb009d2397a8999cd32c
LinZeyuan <903586678@qq.com>
2025-09-04 17:04:48 +0800

    data_revise

--- mmkg/mygo/data_test.py
+++ mmkg/mygo/data_test.py
@@ -17,14 +17,14 @@ from torch import nn
 import logging
 
 from dataset import VTKG
-from model import MyGo
+from model_adjust import MyGo
 from merge_tokens import get_entity_visual_tokens, get_entity_textual_tokens
 from utils import calculate_rank, metrics
 
 """
     代码可复现
 """
-torch.cuda.set_device(1)
+torch.cuda.set_device(0)
 random.seed(0)
 np.random.seed(0)
 torch.manual_seed(0)
@@ -86,7 +86,7 @@ format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s
 stream_handler = logging.StreamHandler(stream=sys.stdout)
 stream_handler.setFormatter(format)
 logger.addHandler(stream_handler)
-file_handler = logging.FileHandler(f'log/{args.model}/{args.data}/log.log')
+file_handler = logging.FileHandler(f'log/{args.model}/{args.data}/test.log')
 file_handler.setFormatter(format)
 logger.addHandler(file_handler)
 
@@ -110,46 +110,25 @@ model = MyGo(args, num_ent=kg.num_ent, num_rel=kg.num_rel, str_dim=args.str_dim,
              visual_dropout=args.visual_dropout, textual_dropout=args.textual_dropout, score_function='tucker').cuda()
 # 模型加载
 # param1 = torch.load(f'ckpt/{args.model}/{args.data}/pre_trained.ckpt')['state_dict']
-# model.load_state_dict(torch.load(f'ckpt/{args.model}/{args.data}/pre_trained_epoch_621_align_wpt_256_0.01_0.01_0.01.ckpt')['state_dict'])
+model.load_state_dict(
+    torch.load(f'ckpt/{args.model}/{args.data}/1400.ckpt')[
+        'state_dict'])
 optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 # 优化器加载
 # param2 = torch.load(f'ckpt/{args.model}/{args.data}/pre_trained.ckpt')['optimizer']
-# optimizer.load_state_dict(torch.load(f'ckpt/{args.model}/{args.data}/pre_trained_epoch_621_align_wpt_256_0.01_0.01_0.01.ckpt')['optimizer'])
+optimizer.load_state_dict(
+    torch.load(f'ckpt/{args.model}/{args.data}/1400.ckpt')[
+        'optimizer'])
 lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=50, T_mult=2)
 # 学习率裁剪器加载
 # param3 = torch.load(f'ckpt/{args.model}/{args.data}/pre_trained.ckpt')['scheduler']
-# lr_scheduler.load_state_dict(torch.load(f'ckpt/{args.model}/{args.data}/pre_trained_epoch_621_align_wpt_256_0.01_0.01_0.01.ckpt')['scheduler'])
-"""
-    模型训练
-"""
-
-
-def train_one_epoch(model, optimizer):
-    model.train()
-    total_loss = 0
-    loss_fn = torch.nn.CrossEntropyLoss()
-    for batch, label in kg_loader:
-        # for batch, label in tqdm(kg_loader):
-        ent_embs, rel_embs, align_before_loss, align_after_loss = model()
-        score = model.score(batch.cuda(), ent_embs, rel_embs)
-        loss = loss_fn(score, label.cuda())
-        if args.align_former is not False:
-            if args.before_align != 0:
-                loss += args.before_align * align_before_loss
-            if args.after_align != 0:
-                loss += args.after_align * align_after_loss
-        if args.contrastive != 0:
-            loss += args.contrastive * model.contrastive_loss(ent_embs)
-        total_loss += loss.item()
-        optimizer.zero_grad()
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=0.1)
-        optimizer.step()
-    return total_loss
+lr_scheduler.load_state_dict(
+    torch.load(f'ckpt/{args.model}/{args.data}/1400.ckpt')[
+        'scheduler'])
 
 
 @torch.no_grad()
-def valid_eval_metric(valid_or_test):
+def valid_eval_metric_1(valid_or_test):
     rank_list = []
     ent_embs, rel_embs, _, _ = model()  # [!!!important]不要放在循环内, 导致测试时速度变慢
     for triple in valid_or_test:
@@ -170,42 +149,36 @@ def valid_eval_metric(valid_or_test):
     return mr, mrr, hit10, hit3, hit1
 
 
-# model.eval()
-# res1 = valid_eval_metric(valid_or_test=kg.valid)
-# print(res1)
-# res2 = valid_eval_metric(valid_or_test=kg.test)
-# print(res2)
-# best_mrr = res2[1] or 0
-best_mrr = 0
+@torch.no_grad()
+def valid_eval_metric_2(valid_or_test):
+    rank_list = []
+    ent_embs, rel_embs, _, _ = model()  # [!!!important]不要放在循环内, 导致测试时速度变慢
+    for triple in valid_or_test:
+        # for triple in tqdm(valid_or_test):
+        h, r, t = triple
+        head_score = \
+            model.score(torch.tensor([[kg.num_ent + kg.num_rel, r + kg.num_ent, t + kg.num_rel]]).cuda(), ent_embs,
+                        rel_embs)[0].detach().cpu().numpy()  # [batch_size, num_entity]
+        head_rank = calculate_rank(head_score, h, kg.filter_dict[(-1, r, t)])
+        rank_list.append(head_rank)
+        logger.info(f"{h}-{r}-{t} tail-rank:{head_rank}")
+        tail_score = \
+            model.score(torch.tensor([[h + kg.num_rel, r + kg.num_ent, kg.num_ent + kg.num_rel]]).cuda(), ent_embs,
+                        rel_embs)[0].detach().cpu().numpy()  # [batch_size, num_entity]
+        tail_rank = calculate_rank(tail_score, t, kg.filter_dict[(h, r, -1)])
+        rank_list.append(tail_rank)
+        logger.info(f"{h}-{r}-{t} tail-rank:{tail_rank}")
+    rank_list = np.array(rank_list)
+    for i in range(0, 8000, 200):
+        mr, mrr, hit10, hit3, hit1 = metrics(rank_list[i:i + 200])
+        logger.info(f"{i}-{i + 200} {mr}-{mrr}-{hit10}-{hit3}-{hit1}")
 
-best_result = None
-for epoch in range(args.num_epoch):
-    loss = train_one_epoch(model, optimizer)
-    lr_scheduler.step()
-    logger.info(f'Epoch {epoch + 1}/{args.num_epoch}, Loss: {loss:.4f}')
-    if (epoch + 1) % args.valid_epoch == 0:
-        model.eval()
-        mr, mrr, hit10, hit3, hit1 = valid_eval_metric(valid_or_test=kg.valid)
-        logger.info("Entity Prediction on Valid Set")
-        logger.info(f"MR: {mr}")
-        logger.info(f"MRR: {mrr}")
-        logger.info(f"Hit10: {hit10}")
-        logger.info(f"Hit3: {hit3}")
-        logger.info(f"Hit1: {hit1}")
-        model.eval()
-        mr, mrr, hit10, hit3, hit1 = valid_eval_metric(valid_or_test=kg.test)
-        logger.info("Entity Prediction on Test Set")
-        logger.info(f"MR: {mr}")
-        logger.info(f"MRR: {mrr}")
-        logger.info(f"Hit10: {hit10}")
-        logger.info(f"Hit3: {hit3}")
-        logger.info(f"Hit1: {hit1}")
-        if mrr > best_mrr:
-            best_mrr = mrr
-            best_result = (mr, mrr, hit10, hit3, hit1)
-            torch.save({'state_dict': model.state_dict(), 'optimizer': optimizer.state_dict(),
-                        'scheduler': lr_scheduler.state_dict()},
-                       f'ckpt/{args.model}/{args.data}/{epoch + 1}.ckpt')
 
-logger.info(f'Best MRR: {best_mrr}, Best Result: {best_result}')
+model.eval()
+res1 = valid_eval_metric_1(valid_or_test=kg.valid)
+print(res1)
+res2 = valid_eval_metric_1(valid_or_test=kg.test)
+print(res2)
+# # best_mrr = res2[1] or 0
+valid_eval_metric_2(kg.test)
 logger.info("Done")
